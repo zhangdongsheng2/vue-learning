1f964e71a49cc10235f67a15ab2219fe6ceb3392
LAPTOP-P90NJHDM\zhangdongsheng <zhangdongsheng2@sina.com>
2022-08-09 18:36:36 +0800

    vue 基础用法

---
+++ vue2_base/js/vue-myPlugin.js
@@ -0,0 +1,23 @@
+(function (window) {
+    const MyPlugin = {}
+    MyPlugin.install = function (Vue,options){
+        // 1. 添加全局方法或属性
+        Vue.myGlobalMethod = function (){
+            console.log('函数对象: myGlobalMethod')
+        }
+
+        // 2. 添加全局资源
+        Vue.directive('my-directive',function (el, binding) {
+            el.textContent = 'my-directive---'+binding.value
+        })
+
+        // 4. 添加实例方法
+        Vue.prototype.$myMethod = function (){
+            console.log("vm $myMethod")
+        }
+
+    }
+
+    window.MyPlugin = MyPlugin
+
+})(window)
